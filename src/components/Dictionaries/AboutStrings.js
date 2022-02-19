22f0be600ff08e85ecfb9fd8f4d9b2c6bfac1c2f
Aevan Candelaria <aevan.candelaria@gmail.com>
2022-02-19 18:49:15 +0800

    Transferred constant strings into a separate js module.

---
+++ src/components/Dictionaries/AboutStrings.js
@@ -0,0 +1,118 @@
+const facultyMembers = [
+	{
+		title: "Dr. Shaneth C. Ambat",
+		shortDescription: "Senior Director for Computer Studies and Multimedia Arts",
+		longDescription: "Dr. Shaneth C. Ambat is currently the Computer Science Program Director at FEU Institute of Technology, Sampaloc Manila. She was once the Dean of Graduate Studies and College of Computer Studies in AMA University, Quezon City. Her research interest was in Self  Organizing  Map,  Reinforcement  Learning  Algorithm,  Fuzzy Logic,   Machine Learning,   Data   Envelopment Analysis, Ontology, Image Processing and Computer Vision.",
+		message: "Dear All Members of the Committees of CS EXPO 2022, Year 2022, the 3rd year of Virtual CS EXPO. With the pandemic, time passed so fast and we survived successfully. I believe that every member of the group has been through hard times and adversities while fulfilling the deliverables expected from you and you were able to leverage all those possibilities through computing technologies. Your experience in Thesis 1 and 2 provided you with a rich foundation in research. Show the world you are not here to just pass through. Leave great footprints wherever you pass and be remembered for the change you initiated. (Israelmore Ayivor)",
+		backgroundImage: "/images/about/faculty/doc_shane400x600.jpg"
+	},
+	{
+		title: "Ms. May San Pablo",
+		shortDescription: "CS Faculty Member",
+		longDescription: "I\'m May Florence D. San Pablo, a Computer Science Professor.  I believe that God is the source of all our blessings.",
+		message: "Congratulations! Thank you for choosing me as your mentor.  I am very proud of you.  Let us thank God for the wisdom and strength He has given us. ",
+		backgroundImage: "/images/about/faculty/ms_may400x600.jpg"
+	},
+	{
+		title: "Mr. Abraham T. Magpantay",
+		shortDescription: "CS Faculty Member",
+		longDescription: "Abraham T. Magpantay is a full-time computer science instructor. He received his graduate degree of Master\'s in Computer Science from the Ateneo de Manila University. His field of interest includes computer vision, image processing, remote sensing, algorithms, and software development. Mr. Magpantay is currently the adviser of the Association for Computing Machinery FEU Tech Student Chapter.",
+		message: "I would like to congratulate everyone for defending and passing their research, especially to my mentees who were able to overcome all the challenges we faced as a group and individually. May you achieve your goals and continually grow as a computer science professional in the field you would choose.",
+		backgroundImage: "/images/about/faculty/magpantay400x600.png"
+	},
+	{
+		title: "Mr. Reginald Cheng",
+		shortDescription: "CS Faculty Member.",
+		longDescription: "Reginald Cheng is a full-time computer science instructor.",
+		message: "No matter how hard the day brings you, there is always a good coffee before the day ends. :DDD",
+		backgroundImage: "/images/about/faculty/sir_cheng.jpg"
+	},
+	{
+		title: "Dr. Beau Gray Habal",
+		shortDescription: "CS Faculty Member.",
+		longDescription: "I am a self-directed and passionate educator who loves to share my knowledge and experiences to my students.  I have established a good academic relationship with my students for the past few years and this has given me joy in life.",
+		message: "To my mentees, who are so diligent, patient, understanding, kind and happy, I wish you all the best in the world.  I hope someday, you will look back and think of me as someone who made a difference in your life.  Always stay safe and reach your dreams!",
+		backgroundImage: "/images/about/faculty/miss_habal.jpg"
+	},
+	{
+		title: "Dr. Rhonnel S. Paculanan",
+		shortDescription: "CS Faculty Member.",
+		longDescription: "Dr. Paculanan is afaculty/ graduate of Doctor in Philosophy major in Education aslo academic completed from the course Doctor of Technology at TUP, graduated BSCS and Master of Science in Computer Science  (MSCS) at AMA computer university, Philippines. He is also a researcher, wherein he published research national and international and one of these is “LPG Leakage Detector using Arduino with SMS Alert and Sound Alarm”. He also attended conference, seminar and workshop here in the Philippines and outside the country. He\'s interest of research is more on Technology specially IoT, Data mining and IT. He is an editorial and technical board member and peer reviewer in some International and National Journals.",
+		message: "",
+		backgroundImage: "/images/about/faculty/sir_paculanan400x600.jpg"
+	}
+];
+
+const facultyTitle = "Faculty";
+
+const committeeMembers = [
+	{
+		title: "Nielsen John M. Bernardo",
+		shortDescription: "CS Expo 2022 Head",
+		longDescription: "Bernardo is the CS Expo 2022 Head. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/bernardo400x600.png"
+	},
+	{
+		title: "Hezekiah John V. Rizan",
+		shortDescription: "CS Expo 2022 Co-Head",
+		longDescription: "Rizan is the co-head for CS Expo 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/rizan400x600.jpg"
+	},
+	{
+		title: "Drake Myer J. Jordan",
+		shortDescription: "Programs Committee Head.",
+		longDescription: "Jordan is the head for the Programs committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/jordan400x600.png"
+	},
+	{
+		title: "Brielle T. Romero",
+		shortDescription: "Creatives Committee Head.",
+		longDescription: "Romero is the head for the Creatives committee. This comittee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/brielle400x600.jpg"
+	},
+	{
+		title: "Justine Rose R. Cale",
+		shortDescription: "Documentations Committee Head.",
+		longDescription: "Cale is the head for the Documentations committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/justine400x600.png"
+	},
+	{
+		title: "Joshua Jerico Simon B. Ferido",
+		shortDescription: "Speakers Committee Head.",
+		longDescription: "Ferido is the head for the Speakers committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/ferido400x600.png"
+	},
+	{
+		title: "Lloyd BJ E. Tarronas",
+		shortDescription: "Sponsors Committee Head.",
+		longDescription: "Tarronas is the head for the Sponsors committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/tarronas400x600.jpg"
+	},
+	{
+		title: "Erika Paula S. Lim",
+		shortDescription: "Logistics Committee Head.",
+		longDescription: "Lim is the head for the Logistics committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/lim400x600.jpg"
+	},
+	{
+		title: "Rom Braveheart P. Leuterio",
+		shortDescription: "Developers - Web Committee Head.",
+		longDescription: "Leuterio is the head for the Developers committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/leuterio400x600.jpg"
+	},
+	{
+		title: "Abraham Israel G. Fabian",
+		shortDescription: "Developers - Publicity Committee Head.",
+		longDescription: "Fabian is the head for the Developers - Publicity committee. This committee is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		backgroundImage: "/images/about/committee/fabian400x600.jpg"
+	}
+];
+
+const committeeTitle = "Committee";
+
+export {
+    facultyMembers,
+    facultyTitle,
+    committeeMembers,
+    committeeTitle
+};
