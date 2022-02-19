0cecd8aef6c10b5b79c485f027e90af515826099
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-02-19 15:35:50 +0800

    UPDATE: Hero images FIX: Gallery page react errors

--- src/components/CarouselGallery.tsx
+++ src/components/CarouselGallery.tsx
@@ -7,54 +7,54 @@ import { useMediaQuery } from 'react-responsive';
 
 
 export default function Carousel({imagesMobile, imageDesktop, slideTitle, bgColor}) {
-
-        const isBigScreen = useMediaQuery({ query: '(min-width: 768px)' })
+    const isBigScreen = useMediaQuery({ query: '(min-width: 768px)' })
 
     return (
-        // @ts-ignore
-        
-            <CarouselComponent className={'flex h-fit py-10 ' + bgColor}
+        <>
+            {/* @ts-ignore */}
+            <CarouselComponent className={'hidden md:flex h-fit py-10 ' + bgColor}
                 showThumbs={false}
                 autoPlay
             >
+                {
+                    imageDesktop.map((imageDesktop) => (
+                        <div key={imageDesktop.backgroundImageFirst + imageDesktop.backgroundImageSecond + imageDesktop.backgroundImageThird}>
+                            <div  className="flex justify-center">
+                                <div className="flex flex-wrap justify-center md:justify-start font-display font-black text-4xl text-[#9147FF]">{slideTitle}</div>
+                            </div>
 
-                {isBigScreen ? (
-
-                        imageDesktop.map((imageDesktop) => (
-                            <div key={imageDesktop}>
-
-                                <div  class="flex justify-center">
-                                    <div class="flex flex-wrap justify-center md:justify-start font-display font-black text-4xl text-[#9147FF]">{slideTitle}</div>
+                            <div className="relative flex space-x-10 px-20 my-20">
+                                <div className="flex-auto">
+                                    <img className="brightness-75" src={imageDesktop.backgroundImageFirst} />
                                 </div>
-                            
-                                <div className="relative flex space-x-10 px-20 my-20">
-                                        <div className="flex-auto">
-                                            <img className="brightness-75" src={imageDesktop.backgroundImageFirst} />
-                                        </div>
-                                        <div className="flex-auto -my-16 ">
-                                            <img className="brightness-75" src={imageDesktop.backgroundImageSecond} />           
-                                        </div>
-                                        <div className="flex-auto">
-                                            <img className="brightness-75" src={imageDesktop.backgroundImageThird} />
-                                        </div>
+                                <div className="flex-auto -my-16 ">
+                                    <img className="brightness-75" src={imageDesktop.backgroundImageSecond} />
+                                </div>
+                                <div className="flex-auto">
+                                    <img className="brightness-75" src={imageDesktop.backgroundImageThird} />
                                 </div>
                             </div>
-                        ))
-                ): 
-                (imagesMobile.map((imagesMobile) => (
-                    <div key={imagesMobile}>
-                                <div  class="flex justify-center my-5">
-                                    <div class="flex flex-wrap justify-center md:justify-start font-display font-black text-4xl text-[#9147FF]">{slideTitle}</div>
-                                </div>
-                                    <img className="brightness-75" src={imagesMobile.backgroundImage} />
                         </div>
-
-                ))) }
-
-
+                    ))
+                }
             </CarouselComponent>
 
-
-            
+            {/* @ts-ignore */}
+            <CarouselComponent className={'flex md:hidden h-fit py-10 ' + bgColor}
+                showThumbs={false}
+                autoPlay
+            >
+                {
+                    imagesMobile.map((imagesMobile) => (
+                        <div key={imagesMobile.backgroundImage}>
+                            <div  className="flex justify-center my-5">
+                                <div className="flex flex-wrap justify-center md:justify-start font-display font-black text-4xl text-[#9147FF]">{slideTitle}</div>
+                            </div>
+                            <img className="brightness-75" src={imagesMobile.backgroundImage} />
+                        </div>
+                    ))
+                }
+            </CarouselComponent>
+        </>
     );
 }
