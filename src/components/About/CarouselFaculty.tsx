ccc306798b516514a8ace3f339386bc597c41f97
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-02-21 23:52:43 +0800

    UPDATE: Responsiveness of carouselFaculty component for HD and FHD screen

--- src/components/About/CarouselFaculty.tsx
+++ src/components/About/CarouselFaculty.tsx
@@ -31,7 +31,7 @@ export default function CarouselFaculty({ images, title, bgColor, reverse = fals
                         </div>
                         <div className={`hidden md:flex md:w-5/6 flex-col justify-center items-center ${reverse ? 'md:items-end' : 'md:items-start'}`}>
                             <h1 className="text-4xl font-display font-bold text-csPrimary text-center w-4/5">{title}</h1>
-                            <div className="p-8 rounded border-2 border-csSecondary w-4/5">
+                            <div className="p-8 rounded border-2 border-csSecondary w-11/12 2xl:w-9/12">
                                 <p className="text-white text-justify font-bold">
                                     {image.longDescription}
                                 </p>
