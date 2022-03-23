7071ee8899930389c91145db3f5c6f7f8e2b0425
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-03-23 09:32:26 +0800

    UPDATE: Files

---
+++ assets/index.6cf20478.js
@@ -0,0 +1,56 @@
+import{_ as e}from"./preload-helper.8b70a8dd.js";import{c as o,o as r}from"./vendor.a4811180.js";o("13ItOf",{value:!0},async()=>{const[{default:n},{default:t}]=await Promise.all([e(()=>import("./CarouselFaculty.c6590002.js"),["assets/CarouselFaculty.c6590002.js","assets/vendor.a4811180.js"]),e(()=>import("./client.e028ff5f.js"),["assets/client.e028ff5f.js","assets/vendor.a4811180.js"])]);return(a,i)=>t(a)(n,{images:[{title:"Ernesto \u2018Boogie\u2019 C. Boydon",shortDescription:"Author and Public Speaker | Computer Scientist | Educator | Techno-Entrepreneur",longDescription:`In 2013, He became the recipient of the
+			Esther A. Vibal Award for Education
+			Technology and Innovation, the first
+			ever in this category, given by the
+			Philippine Association for the
+			Advancement of Science (PhilAAS).
+			PhilAAS, which was established in
+			1951, is a non-profit private national
+			organization that aims to promote
+			and broaden the base of scientific
+			advancement in the country. He was
+			recognized for my work on e-learning
+			systems. In 2020, He ranked #18 of the Top 100
+			Filipinos to Follow on LinkedIn for
+			Inspiration and Learning and was
+			featured in Marketing in Asia.`,message:"",backgroundImage:"/images/speakers/boogie.jpg"},{title:"Ken Mandigma",shortDescription:"Chief Executive Officer",longDescription:`Ken is one the youngest
+        COO in the Philippines
+        having founded 3 ventures
+        at the age of 26, Ken is the
+        current COO of AXS
+        Holdings Corporation and
+        carries 10 years\u2019 worth of
+        business, mobile, and PC
+        game industry experience.
+        He\u2019s been a full time trader
+        of mid-to-large cap stocks,
+        growth stocks, domestic
+        stocks, dividend and non-dividend stocks and
+        cryptocurrency for the
+        past 5 years. He was also
+        recognized as a P1B+ Sales
+        Realtor by Top Tier Real
+        Estate Developers in the
+        Philippines.
+        He's the current COO of
+        8CHAIN, a cryptocurrency
+        project that aims to
+        transform lives one chain
+        at a time.`,message:"",backgroundImage:"/images/speakers/mandigma.jpg"},{title:"Benjoe Vidal",shortDescription:"Founder \u2013 TrashCashPH",longDescription:`8 years working experience in mobile app development for both Android
+        and iOS.
+        Knowledgeable with different Architectural pattern implementation such as
+        VIP Clean Swift, MVC, MVP and MVVM experienced managing multiple
+        projects and working independently with a minimal supervision.
+        Working knowledge of Agile methodology, Object-Oriented Programming,
+        RDBMS, database design, and HTTP data request (XML, JSON, RESTful
+        API\u2019s).
+        Strong foundation and competencies in data structures, algorithms and
+        software design.
+        Knowledgeable in creating unit test script.
+        Working knowledge with integrating SDK and API tools such as Facebook
+        API, Fabrics, Google Map, Paypal, UnionBank payment gateway, in-app
+        purchase, and other third-party libraries.`,message:"",backgroundImage:"/images/speakers/benjoevidal.jpg"},{title:"Calen Martin D. Legaspi",shortDescription:"Chief Executive Officer & Founder, Orange & Bronze Software Labs",longDescription:`In 2005, founded Orange & Bronze Software Labs, the first company in the
+        Philippines, and one of the first companies in Asia, to adopt Agile Software
+        Software Development in its operations, and to offer training and coaching in
+        Agile Software Development. Partnered with Oracle, Google and Pivotal.
+        `,message:"",backgroundImage:"/images/speakers/legaspi.jpg"}],title:"Speaker",bgColor:"bg-csPrimaryDark"},i)});o("1MaWen",{value:!0},async()=>{const[{default:n},{default:t}]=await Promise.all([e(()=>import("./HeroContainer.2bf8fe1e.js"),["assets/HeroContainer.2bf8fe1e.js","assets/vendor.a4811180.js"]),e(()=>import("./client.e028ff5f.js"),["assets/client.e028ff5f.js","assets/vendor.a4811180.js"])]);return(a,i)=>t(a)(n,{},i)});r("1dbyEz",{value:!0},async()=>{const[{default:n},{default:t}]=await Promise.all([e(()=>import("./Header.b8db0b46.js"),["assets/Header.b8db0b46.js","assets/vendor.a4811180.js"]),e(()=>import("./client.e028ff5f.js"),["assets/client.e028ff5f.js","assets/vendor.a4811180.js"])]);return(a,i)=>t(a)(n,{},i)});
